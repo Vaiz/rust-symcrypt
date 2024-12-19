91a112d0ed52e3889fcba834c7f31eb92e3088c3
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-12-19 11:57:03 +0100

    add allow/block lists

--- scripts/generate-bindings.ps1
+++ scripts/generate-bindings.ps1
@@ -25,8 +25,68 @@ $wrapperHeader = '
 
 #include "symcrypt.h"
 '
-
 $wrapperHeader | Out-File -Encoding utf8 -Force -FilePath $header
+
+$varAllowList = @(
+    "^(SYMCRYPT_CODE_VERSION.*)$",
+    "^(SYMCRYPT_(SHA3_256|SHA3_384|SHA3_512|SHA256|SHA384|SHA512|SHA1|MD5)_RESULT_SIZE$)",
+    "^SymCrypt(?:Sha3_(?:256|384|512)|Sha(?:256|384|512|1)|Md5)Algorithm$",
+    "^(SymCryptHmac(Sha256|Sha384|Sha512|Sha1|Md5)Algorithm)$",
+    "SymCryptAesBlockCipher",
+    "SYMCRYPT_AES_BLOCK_SIZE",
+    "^SymCryptEcurveParams(NistP256|NistP384|NistP521|Curve25519)$",
+    "SYMCRYPT_FLAG_ECKEY_ECDH",
+    "SYMCRYPT_FLAG_ECKEY_ECDSA",
+    "SYMCRYPT_FLAG_RSAKEY_ENCRYPT",
+    "SYMCRYPT_FLAG_RSAKEY_SIGN",
+    "SYMCRYPT_FLAG_RSA_PKCS1_NO_ASN1",
+    "SYMCRYPT_FLAG_RSA_PKCS1_OPTIONAL_HASH_OID",
+    "^SymCrypt(Sha(1|256|384|512|3_(256|384|512))|Md5)OidList$"
+)
+$generateVarsParams = @()
+foreach ($var in $varAllowList) {
+    $generateVarsParams += "--allowlist-var" 
+    $generateVarsParams += $var
+}
+
+$functionAllowList = @(
+    "SymCryptModuleInit",
+    "^SymCrypt(?:Sha3_(?:256|384|512)|Sha(?:256|384|512|1)|Md5)(?:Init|Append|Result|StateCopy)?$" ,
+    "^SymCryptHmac(?:Sha(?:256|384|512|1)|Md5)(?:ExpandKey|Init|Append|Result|StateCopy)?$" ,
+    "^(SymCryptGcm(?:ValidateParameters|ExpandKey|Encrypt|Decrypt|Init|StateCopy|AuthPart|DecryptPart|EncryptPart|EncryptFinal|DecryptFinal)?)$" ,
+    "SymCryptChaCha20Poly1305(Encrypt|Decrypt)" ,
+    "^SymCryptTlsPrf1_2(?:ExpandKey|Derive)?$" ,
+    "^SymCryptAesCbc(Encrypt|Decrypt)?$" ,
+    "^SymCryptAesExpandKey$" ,
+    "^(SymCryptHkdf.*)$" ,
+    "^SymCryptEcurve(Allocate|Free|SizeofFieldElement)$" ,
+    "^(SymCryptEckey(Allocate|Free|SizeofPublicKey|SizeofPrivateKey|GetValue|SetRandom|SetValue|SetRandom|))$" ,
+    "SymCryptEcDhSecretAgreement" ,
+    "^SymCryptRsa.*" ,
+    "^(SymCryptEcDsa(Sign|Verify).*)" ,
+    "^(SymCryptRsaPkcs1(Sign|Verify|Encrypt|Decrypt).*)$" ,
+    "^(SymCryptRsaPss(Sign|Verify).*)$" ,
+    "SymCryptWipe" ,
+    "SymCryptRandom" ,
+    "SymCryptLoadMsbFirstUint64" ,
+    "SymCryptStoreMsbFirstUint64"
+)
+$functionBlockList = @(
+     "SymCryptRsakeyCreate" ,
+     "SymCryptRsakeySizeofRsakeyFromParams" ,
+     "SymCryptRsakeyWipe" ,
+     "SymCryptRsaSelftest" ,
+     "^SymCryptRsaRaw.*$" 
+)
+$generateFunctionsParams = @()
+foreach ($function in $functionAllowList) {
+    $generateFunctionsParams += "--allowlist-function" 
+    $generateFunctionsParams += $function
+}
+foreach ($function in $functionBlockList) {
+    $generateFunctionsParams += "--blocklist-function"
+    $generateFunctionsParams += $function
+}
 
 foreach ($target in $targets) {
     if (Test-Path "$outDir/$target") {
@@ -61,6 +121,7 @@ foreach ($target in $targets) {
         $header `
         @bindgenParams `
         --generate vars `
+        @generateVarsParams `
         -o "$outDir/$target/consts.rs" `
         -- @clangParams
 
@@ -69,6 +130,7 @@ foreach ($target in $targets) {
         @bindgenParams `
         --raw-line "use super::types::*;" `
         --generate functions `
+        @generateFunctionsParams `
         -o "$outDir/$target/fns_source.rs" `
         -- @clangParams
 
@@ -78,6 +140,7 @@ foreach ($target in $targets) {
         --raw-line "use super::types::*;" `
         --dynamic-loading APILoader `
         --generate functions `
+        @generateFunctionsParams `
         -o "$outDir/$target/fns_libloading.rs" `
         -- @clangParams
 }
