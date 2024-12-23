f11ed9d1defb16644a94db8f7aa878c88bea8580
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-12-23 13:17:19 +0100

    complile asm files for amd64

---
+++ scripts/process-cppasm.ps1
@@ -0,0 +1,57 @@
+param (
+    [string]$FilePath,
+    [string]$OutFormat,
+    [string]$ArchDefine
+)
+
+# Validate file extension
+$FileExtension = [System.IO.Path]::GetExtension($FilePath)
+if ($FileExtension -ne ".cppasm") {
+    throw "cppasm processing invoked on file with incorrect extension ($FilePath -> $FileExtension)"
+}
+
+# Validate OutFormat
+if ($OutFormat -notin @("gas", "masm", "armasm64")) {
+    throw "cppasm processing invoked with unrecognized outformat ($OutFormat)"
+}
+
+# Validate ArchDefine
+if ($ArchDefine -notin @("amd64", "x86", "arm64", "arm")) {
+    throw "cppasm processing invoked with unrecognized archdefine ($ArchDefine)"
+}
+
+# Get directory and file name components
+$RootPath = [System.IO.Path]::GetDirectoryName($FilePath)
+$FileStem = [System.IO.Path]::GetFileNameWithoutExtension($FilePath)
+$CppAsmArch = "SYMCRYPT_ASM_" + $ArchDefine.ToUpper()
+$OutputAsm = Join-Path $RootPath "$FileStem.asm"
+Write-Output "OutputAsm: $OutputAsm"
+
+# Preprocessing logic based on OutFormat
+if ($OutFormat -eq "gas") {
+    throw "gas outformat not implemented"
+    # GCC-compatible C compiler
+    <#
+    $Command = @(
+        "gcc", "-E", "-P", "-x", "c", $FilePath, "-o", $OutputAsm,
+        @IncludeDirs, "-DSYMCRYPT_GAS", "-D$CppAsmArch", $DbgDefinition
+    )
+    Write-Host "Running GCC preprocessing: $($Command -join ' ')"
+    & $Command
+    #>
+} elseif ($OutFormat -in @("masm", "armasm64")) {
+    # sourced from SymCrypt\msbuild\symcrypt.undocked.props
+    $clPath = & "$PSScriptRoot/get-cl-path.ps1"
+    & $clPath /EP /P `
+        /I "$PSScriptRoot/../symcrypt-sys/upstream/inc" `
+        /I "$PSScriptRoot/../symcrypt-sys/upstream/lib" `
+        "/D$CppAsmArch" `
+        "/DSYMCRYPT_MASM" `
+        "/Fi$OutputAsm" `
+        $FilePath
+} else {
+    throw "Unsupported outformat ($OutFormat)"
+}
+
+Write-Host "C preprocessing of $FilePath completed. Output: $OutputAsm"
+Remove-Item $FilePath
--- scripts/update-upstream.ps1
+++ scripts/update-upstream.ps1
@@ -3,6 +3,9 @@ param(
     [string] $tag = "v103.6.0"
 )
 
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $True
+
 $repoUrl = "https://github.com/microsoft/SymCrypt.git"
 $destinationDir = "$PSScriptRoot/../symcrypt-sys/upstream"
 
@@ -17,7 +20,20 @@ git rev-parse HEAD >> "$destinationDir/VERSION"
 # TODO: move this logic to build.rs
 python "$destinationDir/scripts/version.py" --build-info
 
+# WIN32_amd64
+$symcryptasm = Get-ChildItem "$destinationDir/lib/amd64" -Filter *.symcryptasm;
+foreach ($file in $symcryptasm) {
+    & "$PSScriptRoot/process-symcryptasm.ps1" `
+        -FilePath $file.FullName `
+        -OutFormat masm `
+        -ArchDefine amd64 `
+        -CallingConvention msft
+}
+
 # TODO: cleanup unnecessary files
-$objects_to_keep = @("inc", "lib", "LICENSE", "NOTICE", "README.md", "SECURITY.md", "VERSION", "version.json")
+$objects_to_keep = @(
+    "inc", "lib", "modules",
+    "LICENSE", "NOTICE", "README.md", "SECURITY.md", "VERSION", "version.json"
+)
 Get-ChildItem $destinationDir | Where-Object { $_.Name -notin $objects_to_keep } | Remove-Item -Recurse -Force
 Remove-Item -Recurse -Force "$destinationDir/.git"
