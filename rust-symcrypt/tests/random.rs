4163b248022f698976bf34fdfde3390de5754762
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-01-02 17:41:56 +0100

    cargo fmt

--- rust-symcrypt/tests/random.rs
+++ rust-symcrypt/tests/random.rs
@@ -1,6 +1,6 @@
 #[test]
 fn test_random() {
-    const N : usize = 256;
+    const N: usize = 256;
     let mut buf = [0u8; N];
     symcrypt::symcrypt_random(&mut buf);
     assert_ne!(buf, [0u8; N]);
