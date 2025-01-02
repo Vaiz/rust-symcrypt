43402f9920fe4167d1c61d18d6f7c4479c91ad97
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-01-02 13:03:48 +0100

    compile symcrypt

---
+++ scripts/process-symcryptasm.ps1
@@ -0,0 +1,46 @@
+param (
+    [string]$FilePath,
+    [string]$OutFormat,
+    [string]$ArchDefine,
+    [string]$CallingConvention
+)
+
+# Validate file extension
+$FileExtension = [System.IO.Path]::GetExtension($FilePath)
+if ($FileExtension -ne ".symcryptasm") {
+    throw "symcryptasm processing invoked on file with incorrect extension ($FilePath -> $FileExtension)"
+}
+
+# Validate OutFormat
+if ($OutFormat -notin @("gas", "masm", "armasm64")) {
+    throw "symcryptasm processing invoked with unrecognized outformat ($OutFormat)"
+}
+
+# Validate ArchDefine
+if ($ArchDefine -notin @("amd64", "x86", "arm64", "arm")) {
+    throw "symcryptasm processing invoked with unrecognized archdefine ($ArchDefine)"
+}
+
+# Validate CallingConvention
+if ($CallingConvention -notin @("msft", "systemv", "aapcs64", "arm64ec", "aapcs32")) {
+    throw "symcryptasm processing invoked with unrecognized callingconvention ($CallingConvention)"
+}
+
+# Get directory and file name components
+$RootPath = [System.IO.Path]::GetDirectoryName($FilePath)
+$FileStem = [System.IO.Path]::GetFileNameWithoutExtension($FilePath)
+$OutputCppAsm = Join-Path $RootPath "$FileStem.cppasm"
+Write-Output "OutputCppAsm: $OutputCppAsm"
+
+# Run the Python script
+$PythonScript = "$PSScriptRoot/../symcrypt-sys/upstream/scripts/symcryptasm_processor.py"
+$symcryptasm_args = @(
+    $OutFormat,
+    $ArchDefine,
+    $CallingConvention,
+    $FilePath,
+    $OutputCppAsm
+)
+Write-Output "Running: python $PythonScript $symcryptasm_args"
+& python $PythonScript @symcryptasm_args
+& "$PSScriptRoot/process-cppasm.ps1" $OutputCppAsm $OutFormat $ArchDefine
---
+++ scripts/update-upstream.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [string] $tag = "v103.4.2"
+)
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $True
+
+$repoUrl = "https://github.com/microsoft/SymCrypt.git"
+$destinationDir = "$PSScriptRoot/../symcrypt-sys/upstream"
+
+if (Test-Path $destinationDir) {
+    Remove-Item $destinationDir -Recurse -Force
+}
+git clone --branch $tag --depth 1 $repoUrl $destinationDir
+
+git config --get remote.origin.url > "$destinationDir/VERSION"
+git rev-parse HEAD >> "$destinationDir/VERSION"
+$tag >> "$destinationDir/VERSION"
+
+# TODO: move this logic to build.rs
+python "$destinationDir/scripts/version.py" --build-info
+
+# WIN32_amd64
+$symcryptasm = Get-ChildItem "$destinationDir/lib/amd64" -Filter *.symcryptasm;
+foreach ($file in $symcryptasm) {
+    & "$PSScriptRoot/process-symcryptasm.ps1" `
+        -FilePath $file.FullName `
+        -OutFormat masm `
+        -ArchDefine amd64 `
+        -CallingConvention msft
+}
+
+# TODO: cleanup unnecessary files
+$objects_to_keep = @(
+    "inc", "lib", "modules",
+    "LICENSE", "NOTICE", "README.md", "SECURITY.md", "VERSION", "version.json"
+)
+Get-ChildItem $destinationDir | Where-Object { $_.Name -notin $objects_to_keep } | Remove-Item -Recurse -Force
+Remove-Item -Recurse -Force "$destinationDir/.git"
