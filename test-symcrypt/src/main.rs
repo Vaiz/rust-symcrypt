4dc41726b3031182aaf5ad4b8f10435c9e37e9b2
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-01-07 16:25:35 +0100

    enable multiple symcrypt versions in the same binary

---
+++ test-symcrypt/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+
+    println!("symcrypt_06::is_dynamic_link: {}", symcrypt_06::is_dynamic_link());
+    println!("symcrypt_07::is_dynamic_link: {}", symcrypt_07::is_dynamic_link());
+}
