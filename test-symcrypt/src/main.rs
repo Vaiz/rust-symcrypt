96ed344f3a062846d230242f1c1ef4aee88474c7
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-01-08 13:28:43 +0100

    print build info

--- test-symcrypt/src/main.rs
+++ test-symcrypt/src/main.rs
@@ -8,10 +8,17 @@ fn main() {
     test_random("symcrypt (0.5.0)", symcrypt_05::symcrypt_random);
     test_random("symcrypt (0.6.0)", symcrypt_06::symcrypt_random);
     test_random("symcrypt (0.7.0)", symcrypt_07::symcrypt_random);
+
+    print_build_info("symcrypt (0.6.0)", symcrypt_06::symcrypt_build_info);
+    print_build_info("symcrypt (0.7.0)", symcrypt_07::symcrypt_build_info);
 }
 
 fn test_random(lib: &str, rnd_fn: fn(&mut [u8])) {
     let mut buf = [0u8; 16];
     rnd_fn(&mut buf);
     println!("{lib}: {:?}", buf);
+}
+
+fn print_build_info(lib: &str, build_info_fn: fn() -> &'static str) {
+    println!("{lib}: {}", build_info_fn());
 }
